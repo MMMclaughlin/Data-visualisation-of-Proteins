fd175730039e93e2f6782ca6ff0f764c94424058
b9012721 <b9012721@newcastle.ac.uk>
2019-10-17 10:46:49 +0100

    Changed range to enumerate

--- uniplot/analysis.py
+++ uniplot/analysis.py
@@ -1,10 +1,9 @@
 
 
 def average_len(record):#function to find average protein length
-    print(record)
     totallength = 0#set total length
-    for i in range(0,len(record)):#loop through the list
-        totallength=totallength+len(record[i])#add the length of selected protein
+    for i in enumerate(record):#loop through the list
+        totallength=totallength+len(i[1])#add the length of selected protein
     average=totallength/len(record)#creates average of total/quantity
     return average
 
