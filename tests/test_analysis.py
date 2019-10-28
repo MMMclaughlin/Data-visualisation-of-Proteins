fede89d123482f25278381ef98d05668c14d3578
b9012721 <b9012721@newcastle.ac.uk>
2019-10-28 20:15:42 +0000

    Optimising complete- made a generator
    deleted test_analysis since the test needed parsing and i deleted parsing.py

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,22 +1,19 @@
 import uniplot.analysis
 import uniplot.Generator
-from . import Generator
+from uniplot import Generator
 
 TEST_UNIPROT="./resources/uniprot_sprot_small.xml.gz"
 
 def test_average():
 
-    print(uniplot.analysis.average_len(
-        uniplot.parsing.uniprot_seqrecords(TEST_UNIPROT)
-    ))
 
     records=[]
     gen=Generator.search(TEST_UNIPROT)
     for i in enumerate(gen):#this gets all the proteans
         records.append(len(i[1]))#add all the lengths to a list
-
-    assert uniplot.analysis.average_len(records)
-    == (302.72222222222223)
+    print(uniplot.analysis.average_len(records))
+    assert uniplot.analysis.average_len(records
+    ) == 302.72222222222223
 
 
 
